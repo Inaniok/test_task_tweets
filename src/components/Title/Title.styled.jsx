dafb1c34754bb2c21c758f28ef5046feb21a699b
Serhii Izotov <sergiyizotov@gmail.com>
2023-06-03 18:28:23 +0300

    Create Title.styled.jsx

---
+++ src/components/Title/Title.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const H1 = styled.h1`
+  padding-top: 60px;
+  text-align: center;
+
+  font-weight: 600;
+  font-size: 46px;
+  line-height: 1.4;
+  text-transform: uppercase;
+
+  color: #271c5a;
+`;
